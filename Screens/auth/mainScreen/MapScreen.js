0f9497e0a26f0fad0d246481f917a6ac71e855d8
Olga Salii <saltanoffskaya@gmail.com>
2023-12-18 14:28:41 +0100

    add MapScreen

--- Screens/auth/mainScreen/MapScreen.js
+++ Screens/auth/mainScreen/MapScreen.js
@@ -1,11 +1,36 @@
 // import MapView from "react-native-maps";
 import React, { useState, useEffect } from "react";
+import * as Location from "expo-location";
+
 import { View, Text, Image } from "react-native";
-import { Camera } from "expo-camera";
 
 const MapScreen = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={{ flex: 1 }}>
+      <Text>{text}</Text>
       {/* <MapView
         style={{ flex: 1 }}
         initialRegion={{
